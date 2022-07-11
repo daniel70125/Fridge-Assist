d32b6bab3c087050af43bcfeb26acbbfd16417e8
daniel70125 <dwrighttt504@gmail.com>
2022-07-11 17:13:43 -0500

    Navbar seems to be finished! Hoo-ray!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,11 @@ import './Header.scss';
 import Logo from '../../Pictures/fridge-assist-logo.png';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import {Link} from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
 class Header extends Component {
     state = { 
@@ -56,14 +61,21 @@ class Header extends Component {
                         <div id='user-avatar'>
                             <img id='avatar-img' src='https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png' alt='user avatar'/>
                         </div>
-                        <p>Antoine Anthony</p>
+                        <p>SuperBaker96!</p>
                         <p>New Orleans, LA 70113</p>
                     </div>
-                    <div>
+                    <div className='header-discover-div'>
                         <span>Discover</span>
-                        
+                        <Link className='header-nav-links' to='/'><HomeIcon className='navbar2-icon' /> Home</Link>
+                        <Link className='header-nav-links' to='/products'><SearchIcon className='navbar2-icon' /> Browse</Link>
+                        {/* <Link className='header-nav-links' to='/about'>For You</Link> */}
                     </div>
-                    <div></div>
+                    <div className='header-discover-div'>
+                    <span>Library</span>
+                        <Link className='header-nav-links' to='/'><PersonIcon className='navbar2-icon' /> Chefs</Link>
+                        <Link className='header-nav-links' to='/products'> <FavoriteIcon className='navbar2-icon'/> Favorites</Link>
+                        <Link className='header-nav-links' to='/about'> <MenuBookIcon className='navbar2-icon' /> My Recipes</Link>
+                    </div>
             </div>
            </div>
          );
