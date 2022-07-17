149cdd7f8523be4d6e2879a3c3330e7af58e1198
daniel70125 <dwrighttt504@gmail.com>
2022-07-17 18:55:57 -0500

    Main dish section 1/2 complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Home.scss';
+import {Link} from 'react-router-dom';
+import mainDish from '../../Pictures/main-dish.png';
 
 class Home extends Component {
     state = {  }
     render() { 
         return ( 
             <div>
+                <div id='main-dish-container'>
+                    <img alt='main-dish' src={mainDish} />
+                    <p>Dish <span>of the</span>  Week</p>
+                </div>
             </div>
          );
     }
