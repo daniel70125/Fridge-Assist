4b372e5bf812bf892077f63f09e6c98805ad400f
daniel70125 <dwrighttt504@gmail.com>
2024-06-24 01:22:01 -0500

    working on contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,11 +6,19 @@ class Contact extends Component {
     render() { 
         return ( 
             <div id='contact-wrapper'>
-                <div id='placeeholder-div'></div>
-                <div>
-                    <div>
-                        
-                    </div>
+                <div id='placeholder-div'>
+                    <span>Contact Us</span>
+                </div>
+                <div id='contact-flex-cont'>
+                        <div className='contact-flex-div'>
+                            <p>Div 1</p>
+                        </div>
+                        <div className='contact-flex-div'>
+                            <p>Div 2</p>
+                        </div>
+                        <div className='contact-flex-div'>
+                            <p>Div 3</p>
+                        </div>
                 </div>
             </div>
          );
